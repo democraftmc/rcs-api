04b033c05086c43cfc5e5e45868908be726df179
Funasitien DreamCloud <hello@funa.dev>
2026-01-09 09:11:07 +0100

    ✨ First working local version!

---
+++ src/main/java/fr/democraft/rcs/ClassicLogger.java
@@ -0,0 +1,13 @@
+package fr.democraft.rcs;
+
+public class ClassicLogger {
+    public static String prefix = "SmartRC";
+
+    public void log(String message) {
+        System.out.println("[" + prefix + "] " + message);
+    }
+
+    public void error(String message) {
+        System.out.println("[ERROR] [" + prefix + "] " + message);
+    }
+}
